ada916b0ad7f3503e22bb4e9c8d9135995ed348f
Kome Nathaniel <77756722+nattcodes@users.noreply.github.com>
2024-02-29 21:21:24 +0100

    hello

--- omonwears/src/components/CollectionSection.tsx
+++ omonwears/src/components/CollectionSection.tsx
@@ -1,8 +1,33 @@
-
+import CollectionImga from '../assets/glodi-miessi-ushzBuB_OYY-unsplash.jpg'
+import CollectionImgb from '../assets/pexels-cottonbro-studio-4630675.jpg'
+import CollectionImgc from '../assets/nimble-made-DRM_6zFkPFw-unsplash.jpg'
 
 const CollectionSection = () => {
   return (
-    <div><h1>ddd</h1></div>
+    <section className="services">
+        <div className="services-content">
+          <div className="services-item">
+            <div className="slide-img">
+                <img src={CollectionImga}/>
+                <div className="service-overlay">
+                    <h3>Shoes</h3>
+                </div>
+            </div>
+            <div className="slide-img">
+                <img src={CollectionImgb}/>
+                <div className="service-overlay">
+                    <h3>Trousers</h3>
+                </div>
+            </div>
+            <div className="slide-img">
+                <img src={CollectionImgc}/>
+                <div className="service-overlay">
+                    <h3>Shirts</h3>
+                </div>
+            </div>
+          </div>
+        </div>
+      </section>
   )
 }
 
